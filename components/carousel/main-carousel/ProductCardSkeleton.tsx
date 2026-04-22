40e419a2d0622923350d08738b52b700c2bab3a6
b166er <delagneauadrien@yahoo.fr>
2026-04-22 16:10:05 +0200

    register

--- components/carousel/main-carousel/ProductCardSkeleton.tsx
+++ components/carousel/main-carousel/ProductCardSkeleton.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export function ProductCardSkeleton() {
   return (
-    <div className="flex h-full flex-col overflow-hidden rounded-[1.5rem] bg-white p-3 shadow-sm ring-1 ring-black/5">
+    <div className=" h-full flex flex-col overflow-hidden rounded-[1.5rem] bg-white p-3 shadow-sm ring-1 ring-black/5">
       {/* Image Skeleton */}
       <Skeleton className="aspect-3/4 w-full rounded-2xl" />
 
@@ -15,13 +15,13 @@ export function ProductCardSkeleton() {
         </div>
 
         {/* Description Skeleton */}
-        <div className="mt-3 space-y-2">
-          <Skeleton className="h-3 w-full rounded-md" />
-          <Skeleton className="h-3 w-4/5 rounded-md" />
+        <div className="mt-5 space-y-2">
+          <Skeleton className="h-4 w-full rounded-md" />
+          <Skeleton className="h-4 w-4/5 rounded-md" />
         </div>
 
         {/* Price Skeleton */}
-        <div className="mt-4 flex flex-col space-y-1">
+        <div className="mt-5 flex flex-col space-y-1">
           <Skeleton className="h-3 w-12 rounded-sm" />
           <Skeleton className="h-6 w-20 rounded-md" />
         </div>
